acead8a90f3712819657be174a39053d7e0c073b
Sebastian Tegel <sebastian.tegel@tegelconsulting.se>
2022-10-17 11:06:26 +0200

    Restructure Added js beginning

---
+++ JS/Beginning/src/js/main.js
@@ -0,0 +1,45 @@
+//alert("Hello world!");
+
+let firstname = "Sebastian";
+firstname = "Hanna";
+console.log("Hej" + " " + firstname);
+
+let age = 43;
+//let newAge = age + 1;
+console.log("Age:", age);
+
+age = age + 1;
+console.log(age);
+
+age += 1;
+console.log(age);
+
+age++;
+console.log(age);
+
+age--;
+age -= 1;
+age = age - 1;
+console.log(age);
+
+age = age * 2;
+console.log(age);
+age = age ** 2;
+console.log(age);
+
+age = age / 10;
+console.log(age);
+
+let randomNumber = 10;
+console.log("10 % 2 = ", randomNumber % 2);
+console.log("10 % 5 = ", randomNumber % 5);
+console.log("10 % 3 = ", randomNumber % 3);
+console.log("10 % 9 = ", randomNumber % 9);
+console.log("10 % 4 = ", randomNumber % 4);
+
+console.log("1 % 2 = ", 1 % 2);
+console.log("2 % 2 = ", 2 % 2);
+console.log("3 % 2 = ", 3 % 2);
+console.log("4 % 2 = ", 4 % 2);
+console.log("5 % 2 = ", 5 % 2);
+console.log("6 % 2 = ", 6 % 2);
